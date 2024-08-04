af764dd8885e06cd2a3b4e3f44da004b06241298
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-04 00:58:07 -0300

    feat: Update Input component with icon support and styling improvements

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,24 +1,38 @@
+import React from "react";
+
 interface IInput {
   title?: string;
   type: string;
   placeholder?: string;
   value: string;
+  icon?: React.ComponentType;
+
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ title, type, placeholder, value, onChange }: IInput) {
+export function Input({
+  title,
+  type,
+  placeholder,
+  value,
+  icon: Icon,
+  onChange,
+}: IInput) {
   return (
     <div>
       <label className="block mb-2 text-sm font-medium text-zinc-300">
         {title}
       </label>
-      <input
-        type={type}
-        onChange={onChange}
-        className="block w-full p-2.5 text-base rounded-lg border-gray-500 placeholder-gray-400 text-white"
-        placeholder={placeholder}
-        value={value}
-      />
+      <div className="h-14 flex-1 px-4 bg-zinc-950 border text-zinc-400 border-zinc-800 rounded-lg flex items-center gap-2">
+        {Icon && <Icon />}
+        <input
+          type={type}
+          onChange={onChange}
+          className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+          placeholder={placeholder}
+          value={value}
+        />
+      </div>
     </div>
   );
 }
